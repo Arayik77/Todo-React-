f06ef6184f9887686afb22e2bd6b1f705b392c7c
Arayik77 <arayik_grigoryan_97@mail.ru>
2024-05-14 20:19:14 +0400

    commit

--- src/components/Planets/Planets.tsx
+++ src/components/Planets/Planets.tsx
@@ -4,31 +4,44 @@ import { useEffect, useState } from 'react';
 const Planets = () => {
     const [planets, setPlanets] = useState([]);
     const [error, setError] = useState(null);
+    const [currentPage, setCurrentPage] = useState(1);
 
     useEffect(() => {
         const fetchPlanets = async () => {
           try {
-            const response = await axios.get(`https://swapi.dev/api/planets`);
-            const slicedArray = response.data.results.slice(0, 2);
-            console.log(slicedArray);
-            setPlanets(slicedArray);
+            const response = await axios.get(`https://swapi.dev/api/planets/?page=${currentPage}`);
+            setPlanets(response.data.results);
           } catch (err) {
             // setError(err);
           }
         };
     
         fetchPlanets();
-      }, []);
+      }, [currentPage]);
+
+      const nextPage = () => {
+        setCurrentPage(prevPage => prevPage + 1);
+      };
+    
+      const prevPage = () => {
+        setCurrentPage(prevPage => Math.max(prevPage - 1, 1));
+      };
 
   return (
-    <div style={{display: 'flex',justifyContent: 'space-around'}}>
+    <div>
+      <div style={{display: 'grid',gridTemplateColumns: 'repeat(5, 1fr)',gridGap: '40px'}}>
         {planets.map((planet, index) => (
             <div
-                style={{width: '300px',height: '100px',border: '1px solid black'}}
+                style={{width: '100px',height: '100px',border: '1px solid black'}}
                 key={index}>
                     {planet.name}
             </div>
         ))}
+      </div>
+      <div style={{marginTop: '20px',display: 'flex',justifyContent: 'center'}}>
+          <button onClick={prevPage}>Previous Page</button>
+          <button onClick={nextPage}>Next Page</button>
+      </div>
     </div>
   );
 };
